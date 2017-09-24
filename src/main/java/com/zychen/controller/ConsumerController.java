ffc883df0ffbc536507cd05b1df1b95858d3025c
章源辰 <zhangyuanchen143@163.com>
2017-09-24 23:23:48 +0800

    服务消费者demo

---
+++ src/main/java/com/zychen/controller/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.zychen.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 章源辰
+ * @time: 2017/9/24 22:10
+ * @describion:
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+}
